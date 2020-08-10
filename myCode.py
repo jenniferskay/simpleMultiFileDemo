e10e047076e49b49603545d93b7055f5e17f8ec1
Jennifer S. Kay <kay@rowan.edu>
2020-08-10 17:02:55 -0400

    initial example

---
+++ myCode.py
@@ -0,0 +1,9 @@
+def foo(a, b):
+    print ("a is:", a)
+    print ("b is:", b)
+    return (a*b)
+
+if __name__ == '__main__':
+    print ("c")
+    print (foo(1, 2))
+    
---
+++ testFile.py
@@ -0,0 +1,15 @@
+import myCode
+
+def bar():
+    return 23
+
+if __name__ == '__main__':
+    print ("a")
+    print (foo(1, 2))
+    print ("b")
+    print (bar())
+
+
+
+
+
